1e3ff30fdc0b8675feab4c0881e2dcd1474bfbac
Akshaykumar110904 <ravulakariakshaykumar@gmail.com>
2025-09-07 14:32:33 +0530

    Added Loading animation page to the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import React from 'react';
+import {LoadScreen} from './components/LoadScreen'
 function App() {
+  const [isLoaded, setIsLoaded] = React.useState(false);
   return (
     <>
-      
+      {!isLoaded && <LoadScreen onComplete={()=>setIsLoaded(true)}/>}{" "}
+      <div className={`min-h-screen transition-opacity duration-700 ${isLoaded?"opacity-100":"opacity-0"} bg-black text-gray-100`} >
+      </div>
     </>
   )
 }
